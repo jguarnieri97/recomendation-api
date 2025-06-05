466c85fa0f8e60f27e4c19ec42f51873c09e850d
Ivan Maldonado <82183943+ivanmaldonado09@users.noreply.github.com>
2025-06-05 14:06:29 -0300

    creacion de carpetas

---
+++ src/main/java/ar/edu/unlam/tpi/recomendation_api/service/RecomendationService.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.tpi.recomendation_api.service;
+
+public class RecomendationService {
+    
+}
---
+++ src/main/java/ar/edu/unlam/tpi/recomendation_api/service/impl/RecomendationServiceImpl.java
@@ -0,0 +1,5 @@
+package ar.edu.unlam.tpi.recomendation_api.service.impl;
+
+public class RecomendationServiceImpl {
+    
+}
