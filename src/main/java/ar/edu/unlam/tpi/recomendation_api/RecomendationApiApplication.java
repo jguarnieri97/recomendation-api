6805f4269d6cdbd3c7f3114ad69403eae53a0ecc
Julián Guarnieri <jguarnieri@alumno.unlam.edu.ar>
2025-06-05 11:50:12 -0300

    Base project

---
+++ src/main/java/ar/edu/unlam/tpi/recomendation_api/RecomendationApiApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tpi.recomendation_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecomendationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecomendationApiApplication.class, args);
+	}
+
+}
