01d2134a7ad392e1ac3927fef3e163925ee8a6d4
Julián Guarnieri <jguarnieri@alumno.unlam.edu.ar>
2025-06-10 10:14:21 -0300

    Fix proceso de recomendación

--- src/main/java/ar/edu/unlam/tpi/recomendation_api/service/impl/RecommendationServiceImpl.java
+++ src/main/java/ar/edu/unlam/tpi/recomendation_api/service/impl/RecommendationServiceImpl.java
@@ -1,10 +1,13 @@
 package ar.edu.unlam.tpi.recomendation_api.service.impl;
 
+import ar.edu.unlam.tpi.recomendation_api.dto.request.TagRequest;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
@@ -38,25 +41,32 @@ public class RecommendationServiceImpl implements RecommendationService {
     @Override
     public void createRecommendation(RecommendationRequest request) {
         log.debug("Creando recomendaciones para applicantId {}", request.getApplicantId());
-    
-        List<RecommendationEntity> toSave = request.getTags().stream()
+
+        List<RecommendationEntity> toSave = new ArrayList<>();
+        List<TagRequest> tags = request.getTags().stream()
                 .peek(tag -> log.debug(" Revisando tag: {} con probabilidad {}", tag.getTagName(), tag.getProbability()))
                 .filter(tag -> {
                     boolean isAboveThreshold = tag.getProbability() >= threshold;
                     log.debug(" Pasa threshold {}: {}", threshold, isAboveThreshold);
                     return isAboveThreshold;
                 })
-                .map(tag -> {
-                    Optional<String> categoria = config.getCategoryByTag(tag.getTagName());
-                    if (categoria.isEmpty()) {
-                        log.debug("Tag '{}' no tiene categoría configurada", tag.getTagName());
-                    } else {
-                        log.debug("Tag '{}' pertenece a categoría '{}'", tag.getTagName(), categoria.get());
-                    }
-                    return categoria.map(cat -> converter.convertToEntity(request.getApplicantId(), tag)).orElse(null);
-                })
-                .filter(Objects::nonNull)
                 .toList();
+
+        for (TagRequest tag : tags) {
+            Optional<String> categoria = config.getCategoryByTag(tag.getTagName());
+            if (categoria.isEmpty()) {
+                log.debug("Tag '{}' no tiene categoría configurada", tag.getTagName());
+            } else {
+                log.debug("Tag '{}' pertenece a categoría '{}'", tag.getTagName(), categoria.get());
+                toSave.add(converter.convertToEntity(request.getApplicantId(), tag));
+            }
+        }
+
+        if (toSave.isEmpty()) {
+            log.warn("No se encontraron recomendaciones. Se crea una por defecto para applicantId {}", request.getApplicantId());
+            RecommendationEntity fallback = fallbackUtils.createDefaultCleaningRecommendation(request.getApplicantId());
+            toSave.add(fallback);
+        }
     
         log.debug("💾 Cantidad de recomendaciones a guardar: {}", toSave.size());
         toSave.forEach(r -> log.debug("💾 Guardando: applicantId={}, tag={}, prob={}", r.getApplicantId(), r.getTag(), r.getProbability()));
@@ -69,13 +79,7 @@ public class RecommendationServiceImpl implements RecommendationService {
     public List<RecommendationResponse> getRecommendations(Long applicantId, int limit) {
         Pageable pageable = PageRequest.of(0, limit);
         List<RecommendationEntity> entities = dao.findByApplicantIdOrderByCreatedAtDesc(applicantId, pageable);
-    
-        if (entities.isEmpty()) {
-            log.warn("No se encontraron recomendaciones. Se crea una por defecto para applicantId {}", applicantId);
-            RecommendationEntity fallback = fallbackUtils.createDefaultCleaningRecommendation(applicantId);
-            entities = List.of(fallback);
-        }
-    
+
         return entities.stream()
                 .map(rec -> {
                     String cat = config.getCategoryByTag(rec.getTag()).orElse("UNKNOWN");
